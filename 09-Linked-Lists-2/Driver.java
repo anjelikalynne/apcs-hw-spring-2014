34f80d6c3d5834d163592b8261a3d6bab5a1ad34
anjelikalynne <anjelikalynne.amog@cslab1-2.stuy.edu>
2014-03-26 08:10:40 -0400

    iterator

--- 09-Linked-Lists-2/Driver.java
+++ 09-Linked-Lists-2/Driver.java
@@ -1,31 +1,16 @@
 import java.io.*;
 import java.util.*;
 
-public class Driver {
+public class Driver{
     public static void main(String[] args){
-	MyLinkedList mll = new MyLinkedList();
-	mll.add("Hercules");
-	mll.add("Philoctetes");
-	mll.add("Pegasus");
-	System.out.println("The original length: " + mll.length());
-	System.out.println("This is the first list:\n " + mll);
-	mll.add("Megara",1);
-	System.out.println();
-	System.out.println("After adding Megara at position 1:\n" + mll);
-	System.out.println("The new length: " + mll.length());
-	System.out.println("At position 1: " + mll.get(1));
-	System.out.println("At position 0: " + mll.get(0));
-	String s = mll.set(2, "Hera");
-	System.out.println();
-	System.out.println("old string at position 2: " + s);
-	System.out.println();
-	System.out.println("After replacing Philoctetes at position 2 with Hera:\n" + mll);
-	s = mll.remove(2);
-	System.out.println();
-	System.out.println("removed item at position 2: " + s);
-	System.out.println("After removing Hera:\n" + mll);
-	System.out.println("The length: " + mll.length());
-	System.out.println("Finding Hera: " + mll.find("Hera"));
-	System.out.println("Finding Meg: " + mll.find("Megara"));
+	MyLinkedList L = new MyLinkedList();
+	L.add("one");
+	L.add("two");
+	L.add("three");
+	
+	Iterator<String> it = L.iterator();
+	while (it.hasNext()){
+	    System.out.println(it.next());
+	}
     }
 }
