810d4772c1af6fef964a177942158467ca3c19ac
anjelikalynne <anjelikalynne.amog@cslab1-2.(none)>
2014-04-07 08:34:02 -0400

    pancake folder

---
+++ Stack/Driver.java
@@ -0,0 +1,12 @@
+public class Driver{
+    public static void main(String[] args){
+	MyStack stack = new MyStack();
+	stack.push("one");
+	stack.push("two");
+	stack.push("three");
+	System.out.println(stack);
+	while (!stack.isEmpty()){
+	    System.out.println(stack.pop());
+	}
+    }
+}
