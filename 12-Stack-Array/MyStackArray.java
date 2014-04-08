18e187759d4e5798a52cc43ec6269e92e090e056
anjelikalynne <anjelikalynne.amog@cslab1-2.(none)>
2014-04-08 08:32:38 -0400

    pancakes

--- 12-Stack-Array/MyStackArray.java
+++ 12-Stack-Array/MyStackArray.java
@@ -3,52 +3,48 @@ import java.util.*;
 
 public class MyStackArray {
     private String[] stackArray;
-    private String top;
-    private int len;
+    private int top;
 
     public MyStackArray(){
 	stackArray = new String[10];
-	top = null;
-	len = 0;
+	top = -1;
     }
 
     public void push(String s){
-	for (int i = len; i > 0; i--){
-	    stackArray[i + 1] = stackArray[i];
-	}
-	stackArray[0] = s;
-	top = stackArray[0];
-	len++;
+	if (top > stackArray.length)
+	    grow();
+	top++;
+	stackArray[top] = s;
     }
 
     public String peek(){
-	return top;
+	return stackArray[top];
     }
 
     public String pop(){
-	String popped = top;
-	for (int i = 0; i < len; i++){
-	    stackArray[i] = stackArray[i+1];
-	}
+	String popped = peek();
+	top--;
 	return popped;
     }
 
     public void grow(){
 	int l = stackArray.length;
 	String[] tmp = new String[l + (l/2)];
-	for (int i = 0; i < len; i++){
+	for (int i = 0; i <= top; i++){
 	    tmp[i] = stackArray[i];
 	}
 	stackArray = tmp;
     }
 
     public String toString(){
-	int i = 0;
 	String s = "";
-	while (i < len){
-	    s = s + stackArray[i] + " ,";
+	if (top >= 0){
+	    for (int i = top; i >= 0; i--){
+		s = s + stackArray[i] + ", ";
+	    }
 	}
 	return s;
+
     }
     
 }
