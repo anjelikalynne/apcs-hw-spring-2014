6e9e853c5644b2cfd0c226baa9e60bf57f9d12c2
anjelikalynne <anjelikalynne.amog@cslab1-2.(none)>
2014-04-24 08:40:38 -0400

    BST - how to use dummy node for insert?

---
+++ BinTree/BinSearchTree.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class BinSearchTree{
+    private Node root;
+
+    public Node search(int x){
+	
+    }
+
+    public Node search(Node P, int x){
+
+    }
+
+
+}
